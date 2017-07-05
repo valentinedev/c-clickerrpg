fdbc784b84dc8af3d6fd22d08f690541d751f043
valentinedev <exkuikui@gmail.com>
2017-07-04 20:18:05 -0700

    Add files via upload
    Update 1: Added Rebirth system. There is currently an issue where the ints quickly become too large and overflow.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <windows.h>
+#include <limits>
 
 using namespace std;
 
@@ -17,11 +18,11 @@ class Character
 
 Character::Character()
 {
-    points = 1;
 }
 
 Character::~Character()
 {
+    points = 0;
 }
 
 void Character::setpoints(int s)
@@ -32,16 +33,26 @@ void Character::setpoints(int s)
 int Character::printstats()
 {
     cout<<points;
+    return 0;
 }
+
+int rebirth();
+
+int cost;
+int goal;
+int currency;
+int rebirthprice;
 
 int main()
 {
     char pinput[20];
     char playername[20];
     int pointadd;
-    int cost;
     pointadd = 1;
     cost = 10;
+    goal = 100;
+    currency = 0;
+    rebirthprice = 5;
     while(1)
     {
         cout<<"Enter Name: ";
@@ -51,7 +62,7 @@ int main()
             system("CLS");
             cout<<"Invalid Input\n";
             cin.clear();
-            cin.ignore(10000,'\n');
+            cin.ignore(std::numeric_limits<streamsize>::max(),'\n');
             continue;
         }
         if(strcmp(playername,"")==0)
@@ -64,20 +75,22 @@ int main()
     }
     system("CLS");
     Character player;
+    player.points = 0;
     while(1)
     {
         while(1)
         {
             cout<<playername<<"'s Points:\n";
             player.printstats();
-            cout<<"\n\nBlank. Click\n\n1. Points Per Click X2: Points Per Click: "<<pointadd*2<<" Cost: "<<cost<<"\n\nInput: ";
+            cout<<"\nGoal: "<<goal<<"\nCurrency: "<<currency;
+            cout<<"\n\nBlank. Click\n\n1. Points Per Click X2: Points Per Click: "<<pointadd*2<<" Cost: "<<cost<<"\n\n2. Rebirth. Currency Required: "<<rebirthprice<<"\n\nInput: ";
             cin.getline(pinput,20,'\n');
             if(cin.fail())
             {
                 system("CLS");
                 cout<<"Invalid Input\n";
                 cin.clear();
-                cin.ignore(10000,'\n');
+                cin.ignore(std::numeric_limits<streamsize>::max(),'\n');
                 continue;
             }
             system("CLS");
@@ -103,5 +116,37 @@ int main()
                 cout<<"Invalid Input. Not Enough Points.\n";
             }
         }
+        else if(strcmp(pinput,"2")==0)
+        {
+
+            if(rebirthprice<=currency)
+            {
+                system("CLS");
+                currency -= rebirthprice;
+                rebirth();
+                player.points = 0;
+                continue;
+            }
+            else
+            {
+                system("CLS");
+                cout<<"Invalid Input. Not Enough Currency.\n";
+            }
+        }
+        else{}
+        if(player.points>=goal)
+        {
+            currency += 1;
+            goal *= 5;
+        }
     }
+}
+
+int rebirth()
+{
+    cost = 10;
+    goal = 100;
+    currency = 0;
+    rebirthprice *= 3;
+    return 0;
 }
